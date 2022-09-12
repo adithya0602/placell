9dee33cbf4460364b62d5bff81cc51f8ffea3a7b
Manish Sagar <adithyaravirao@gmail.com>
2022-09-12 16:03:20 +0530

    commit

--- first/first/settings.py
+++ first/first/settings.py
@@ -57,7 +57,7 @@ ROOT_URLCONF = 'first.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ["templates"],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -119,8 +119,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = '/static/'
-MEDIA_ROOT=os.path.join(BASE_DIR,'first/media')
-STATIC_ROOT=os.path.join(BASE_DIR,'first/static')
+MEDIA_ROOT=os.path.join(BASE_DIR,'media')
+STATIC_ROOT=os.path.join(BASE_DIR,'static')
 MEDIA_URL='/media/'
 STATIC_ROOT='/static/'
 
