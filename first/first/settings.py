4977db85786519bdf3afcc481bfe4d0051f2f770
Manish Sagar <adithyaravirao@gmail.com>
2022-10-22 21:14:42 +0530

    commit

--- first/first/settings.py
+++ first/first/settings.py
@@ -120,9 +120,8 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 MEDIA_ROOT=os.path.join(BASE_DIR,'media')
-STATIC_ROOT=os.path.join(BASE_DIR,'static')
+STATIC_ROOT=os.path.join(BASE_DIR,'assets')
 MEDIA_URL='/media/'
-STATIC_ROOT='/static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
@@ -130,6 +129,6 @@ STATIC_ROOT='/static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 #added manually
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static")
+    os.path.join(BASE_DIR, "first\static")
 ]
 AUTH_USER_MODEL='hello.User'
